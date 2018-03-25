c63f1494d239464cd921a8253f91acdf80de49b2
Soumya <soumyajyoti.jh@tamu.edu>
2018-03-25 02:37:54 +0000

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,23 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-question_banks = [{:category => 'Networks', :question => 'In the Internet Protocol (IP) suite of protocols, which of the following best describes the purpose of the Address
-Resolution Protocol?', :option1 => 'To translate Web addresses to host names', :option2 => 'To determine the IP address of a given host name', :option3 => 'To determine the hardware address of a given host name', :option4 => 'To determine the hardware address of a given IP address', :option5 => 'To determine the appropriate route for a datagram', :answer => 2},
-		  {:category => 'Algorithms', 
-		   :question => 'Let X be a problem that belongs to the class NP. Then which one of the following is TRUE?',
- 		   :option1 => 'There is no polynomial time algorithm for X',
-		   :option2 => 'If X can be solved deterministically in polynomial time, then P = NP', 
-		   :option3 => 'If X is NP-hard, then it is NP-complete', 
-		   :option4 => 'X may be undecidable', 
-		   :option5 => 'None of the above', 
-		   :answer => 3}
-  	 	]
+question_banks = [
+	{	:category => 'Networks',
+		:question => 'In the Internet Protocol (IP) suite of protocols, which of the following best describes the purpose of the Address Resolution Protocol?',
+		:option1 => 'To translate Web addresses to host names',
+		:option2 => 'To determine the IP address of a given host name',
+		:option3 => 'To determine the hardware address of a given host name',
+		:option4 => 'To determine the hardware address of a given IP address',
+		:option5 => 'To determine the appropriate route for a datagram', :answer => 2},
+	{	:category => 'Algorithms', 
+		:question => 'Let X be a problem that belongs to the class NP. Then which one of the following is TRUE?',
+		:option1 => 'There is no polynomial time algorithm for X',
+		:option2 => 'If X can be solved deterministically in polynomial time, then P = NP', 
+		:option3 => 'If X is NP-hard, then it is NP-complete', 
+		:option4 => 'X may be undecidable', 
+		:option5 => 'None of the above', 
+		:answer => 3}
+]
 
 question_banks.each do |question|
   QuestionBank.create(question)
