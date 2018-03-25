2fc03f8f71f42d4c62eb88a62576620bd32b90fc
Soumya <soumyajyoti.jh@tamu.edu>
2018-03-25 07:40:33 +0000

    rspec

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -13,10 +13,15 @@ describe AdminController do
   end
   
   describe "#index" do
-    #let!(:question1) {FactoryBot.create(:questionbank, question: 'Test Question1',category: 'RSpec Test1',option1: '1', option2: '2', option3: '3', option4: '4', option5: '5', answer: '2')}
-    #let!(:question2) {FactoryBot.create(:questionbank, question: 'Test Question2',category: 'RSpec Test2',option1: '1', option2: '2', option3: '3', option4: '4', option5: '5', answer: '3')}
-    it "get all questions" do
+    it "gets all questions" do
       get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+  
+  describe "#show" do
+    it "tests show method" do
+      get :show
       expect(response).to have_http_status(:success)
     end
   end
