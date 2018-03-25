790e1510d415b45d3f9d8a68a05c54418f20a197
EC2 Default User <ec2-user@ip-172-31-11-103.us-east-2.compute.internal>
2018-03-25 03:14:27 +0000

    added initial spec testcase

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 describe AdminController do
-  describe 
+
+  describe 'Create Question' do
+
+    it 'should create a question entry' do
+      post :create, question: { question: 'Test Question',category: 'RSpec Test',option1: '1', option2: '2', option3: '3', option4: '4', option5: '5', answer: '2'}
+      expect(flash[:notice]).to match(/Question was successfully added/)
+      expect(response).to redirect_to(admin_path)
+    end
+
+  end
+
 end
