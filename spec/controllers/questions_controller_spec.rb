8ff15b31e8f4a074db786c38ee3363a4aad91cdf
EC2 Default User <ec2-user@ip-172-31-11-103.us-east-2.compute.internal>
2018-04-06 01:41:50 +0000

    rspec testcases Iter2

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
+require 'factory_bot'
 
 RSpec.describe QuestionsController, type: :controller do
-
+    
+    #For create
+    describe "Create Question" do
+        let "adds a new question" do
+            post :create, question: {question: 'Test Question',category: 'RSpec Test',option1: '1', option2: '2', option3: '3', option4: '4', option5: '5', answer: '2'}
+            expect(flash[:notice]).to match(/was successfully created/)
+            expect(response).to redirect_to(questions_path)
+        end
+    end
+    
+    #For update 
+    describe "Update Question" do
+        let!(:question1) { FactoryBot.create(:question, question: 'Test Question', category: 'RSpec Test', option1: '1', option2: '2', option3: '3', option4: '4', option5: '5', answer: '2')}
+        it "should update the attributes" do
+            put :update, {id: question1.id, :question => { 'option2' => '22'}}
+            expect(flash[:notice]).to match(/was successfully created/)
+            expect(response).to redirect_to(questions_path)
+        end
+    end
+    
+    #For Destroy
+    describe "Destory Question" do
+        let!(:question1) { FactoryBot.create(:question, question: 'Test Question',category: 'RSpec Test',option1: '1', option2: '2', option3: '3', option4: '4', option5: '5', answer: '2')}
+        it "should delete the question" do
+            delete :destroy, {id: question1.id}
+            expect(flash[:notice]).to match(/deleted/)
+            expect(response).to redirect_to(questions_path)
+        end
+    end
+    
+    #For user confimation
+    describe "Confirm user" do
+        it "should check user validity" do
+            logged_in = false
+            expect(flash[:notice]).to match(/Please log in/)
+            expect(response).to redirect_to(login_url)
+        end
+  end
+    
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,5 +8,38 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
+  
+  describe "show function" do
+    it "shows questions" do
+      expect(response).to redirect_to(questions_path)
+    end
+  end
+  
+  #incomplete
+  describe "Update User" do
+    let!(:user1) { FactoryBot.create(:user, name: 'test1', email: 'test@testing.com', password: 'testing')}
+    it "updates the profile" do
+      put :update, {id: user1.id, :user => { 'name' => 'test2'}}
+      expect(flash[:notice]).to match(/Profile updated/)
+    end
+  end
+  
+  describe "User confirm " do
+    it "logged_in_user" do
+      logged_in = false
+      expect(flash[:notice]).to match(/Please log in/)
+      expect(response).to redirect_to(login_url)
+    end
+  end
+  
+  #incomplete
+  describe "check correctness" do
+    let!(:user1) { FactoryBot.create(:user, name: 'test1', email: 'test@testing.com', password: 'testing')}
+    it "confirms correct user" do
+      get :edit, {id: user1.id}
+      # unless @user == current_user ??
+      expect(response).to redirect_to(root_url) 
+    end
+  end
+  
 end
