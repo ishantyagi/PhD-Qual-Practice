e67ec491367c2348a45725087d61e255a560ee43
Vineet <vineet.bnl@gmail.com>
2018-04-17 23:03:01 +0000

    Removed redundancy in selcat controller

--- app/controllers/selcat_controller.rb
+++ app/controllers/selcat_controller.rb
@@ -2,8 +2,6 @@ class SelcatController < ApplicationController
     
     def index
         @all_categories = QuestionBank.category
-        @sort_by = params[:sort_by]
-        @Questions = QuestionBank.order(@sort_by)
     end
     
 end
